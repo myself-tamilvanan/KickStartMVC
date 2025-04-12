a8b01f19972c1866d7d4fe03b8b990a6f0617e62
Tamilvanan R <contact.tamizhvanan@gmail.com>
2025-04-12 16:03:00 +0530

    Edit and Delete Category Implementation

--- KickStartMVC/Controllers/CategoryController.cs
+++ KickStartMVC/Controllers/CategoryController.cs
@@ -18,6 +18,11 @@ namespace KickStartMVC.Controllers
             return View(categories);
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
         [HttpPost]
         public IActionResult Create(CategoryModel category) {
 
@@ -40,6 +45,75 @@ namespace KickStartMVC.Controllers
 
             return View(); 
         }
+        
+        public IActionResult Edit(int? Id)
+        {
+            if (Id == null && Id == 0)
+            {
+                return NotFound();
+            }
 
+            CategoryModel? category = _db.Categories.Find(Id);
+            //CategoryModel? category1 = _db.Categories.FirstOrDefault(item => item.Id == Id);
+            //CategoryModel? category2 = _db.Categories.Where(item => item.Id == Id).FirstOrDefault();
+
+            if (category == null)
+            {
+                NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(CategoryModel category) {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(); 
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? Id) {
+            if (Id == null && Id == 0)
+            {
+                return NotFound();
+            }
+
+            CategoryModel? category = _db.Categories.Find(Id);
+
+            if (category == null)
+            {
+                NotFound();
+            }
+            else
+            {
+                _db.Categories.Remove(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+
+            return View(); 
+        }
+        
+        public IActionResult Delete(int? Id)
+        {
+            if (Id == null && Id == 0)
+            {
+                return NotFound();
+            }
+
+            CategoryModel? category = _db.Categories.Find(Id);
+
+            if (category == null)
+            {
+                NotFound();
+            }
+
+            return View(category);
+        }
     }
 }
